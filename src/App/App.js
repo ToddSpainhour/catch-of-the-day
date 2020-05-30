2bed8156a013442324d84e408bdcb229e678c84d
Todd Spainhour <toddspainhour@gmail.com>
2020-05-29 20:48:06 -0500

    removed setup text and icon

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import StorePicker from '../components/StorePicker/StorePicker';
 import './App.scss';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info"><i className="fas fa-rocket"></i></button>
+        <StorePicker />
       </div>
     );
   }
